ef0c6f584f6b75271f61857fccec1b0be5595ff4
vishal1221-1221 <65186356+vishal1221-1221@users.noreply.github.com>
2022-05-19 19:08:53 +0530

    hello

---
+++ Controllers/UserLoginController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserLogin.Database;
+using UserLogin.Database.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLoginController : ControllerBase
+    {
+
+      
+        public List<UserModel> usersList = new List<UserModel>()
+        {
+            new UserModel
+            {
+                UserName="gopal",
+                Password="gopal"
+            },
+            new UserModel
+            {
+                UserName="Vishal",
+                Password="Vishal123"
+            },
+            new UserModel
+            {
+                UserName="Ram",
+                Password="Ram123"
+            }
+        };
+        
+
+        // [System.Web.Http.Route("Login")]
+
+        [HttpPost]
+        public IActionResult userLogin(UserModel login)
+        {
+
+            //var log = ln.Login(login.UserName, login.Password);
+            //var log = db.users.Where(x => x.UserName.Equals(login.UserName) && x.Password.Equals(login.Password)).FirstOrDefault();
+            var log = usersList.Find(x => x.UserName == login.UserName && x.Password == login.Password);
+            if (log == null)
+            {
+                return Ok(new { Status = "Invalid", Message = "Invalid User" });
+            }
+            else
+            {
+
+
+
+                return Ok(new { Status = "Success", Message = "Login Success",});
+            }
+        }
+
+    }
+}
---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserLogin.Database.Entity;
+
+namespace UserLogin.Database
+{
+    public class DatabaseContext:DbContext
+
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<UserModel> users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().HasData(
+                new UserModel
+                {
+                    UserName = "gopal",
+                    Password = "gopal"
+                }
+
+                );
+        }
+
+    }
+}
